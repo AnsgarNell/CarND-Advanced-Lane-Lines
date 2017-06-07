03bb85eca41afdcdda146fe4ebcc21d483f92256
Ansgar Nell <ansgar.nell@gmail.com>
2017-06-07 15:33:45 +0200

    detect_new_line implemented

--- advanced_lane_lines.py
+++ advanced_lane_lines.py
@@ -6,28 +6,14 @@ import os
 import matplotlib.pyplot as plt
 
 # Define a class to receive the characteristics of each line detection
-class Line():
-    def __init__(self):
-        # was the line detected in the last iteration?
-        self.detected = False  
-        # x values of the last n fits of the line
-        self.recent_xfitted = [] 
-        #average x values of the fitted line over the last n iterations
-        self.bestx = None     
-        #polynomial coefficients averaged over the last n iterations
-        self.best_fit = None  
-        #polynomial coefficients for the most recent fit
-        self.current_fit = [np.array([False])]  
-        #radius of curvature of the line in some units
-        self.radius_of_curvature = None 
-        #distance in meters of vehicle center from the line
-        self.line_base_pos = None 
-        #difference in fit coefficients between last and new fits
-        self.diffs = np.array([0,0,0], dtype='float') 
-        #x values for detected line pixels
-        self.allx = None  
-        #y values for detected line pixels
-        self.ally = None
+class Lane():
+	def __init__(self):
+		self.detected = False  
+		self.left_fit = None
+		self.right_fit = None  
+		self.radius_of_curvature = None 
+		#distance in meters of vehicle center from the lane
+		self.lane_center_meters = None 
 
 # Read in the saved camera matrix and distortion coefficients
 # These are the arrays you calculated using cv2.calibrateCamera()
@@ -36,8 +22,11 @@ mtx = dist_pickle["mtx"]
 dist = dist_pickle["dist"]
 input_folder = './test_images/'
 output_folder = './output_images/'
+filename = 'test_image.jpg'
 # Make a list of calibration images
 images = glob.glob(input_folder + '*.jpg')
+save_images = True
+is_video = False
 
 # Combined color and gradient thresholds
 def hls_select(img, s_thresh=(170, 255), sx_thresh=(20, 100), l_thresh=(40,255)):
@@ -84,20 +73,18 @@ def transform(img):
 	warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
 	return warped, Minv
 	
-def detect_lines(binary_warped, filename):
+def detect_lines(binary_warped,lane):
 	# Assuming you have created a warped binary image called "binary_warped"
 	# Take a histogram of the bottom half of the image
 	histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
 	
-	# Uncomment for saving images
-	"""
-	# Save the histogram
-	plt.plot(histogram)
-	write_name = './output_images/histogram_' + filename
-	plt.savefig(write_name)
-	plt.clf()
-	"""
-	
+	if save_images:
+		# Save the histogram
+		plt.plot(histogram)
+		write_name = output_folder + 'histogram_' + filename
+		plt.savefig(write_name)
+		plt.clf()
+
 	# Create an output image to draw on and  visualize the result
 	out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
 	# Find the peak of the left and right halves of the histogram
@@ -119,57 +106,68 @@ def detect_lines(binary_warped, filename):
 	rightx_current = rightx_base
 	# Set the width of the windows +/- margin
 	margin = 100
-	# Set minimum number of pixels found to recenter window
-	minpix = 50
-	# Create empty lists to receive left and right lane pixel indices
-	left_lane_inds = []
-	right_lane_inds = []
+	
+	if not(lane.detected):
+		# Set minimum number of pixels found to recenter window
+		minpix = 50
+		# Create empty lists to receive left and right lane pixel indices
+		left_lane_inds = []
+		right_lane_inds = []
 
-	# Step through the windows one by one
-	for window in range(nwindows):
-		# Identify window boundaries in x and y (and right and left)
-		win_y_low = binary_warped.shape[0] - (window+1)*window_height
-		win_y_high = binary_warped.shape[0] - window*window_height
-		win_xleft_low = leftx_current - margin
-		win_xleft_high = leftx_current + margin
-		win_xright_low = rightx_current - margin
-		win_xright_high = rightx_current + margin
-		# Draw the windows on the visualization image
-		cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
-		cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
-		# Identify the nonzero pixels in x and y within the window
-		good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-		good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
-		# Append these indices to the lists
-		left_lane_inds.append(good_left_inds)
-		right_lane_inds.append(good_right_inds)
-		# If you found > minpix pixels, recenter next window on their mean position
-		if len(good_left_inds) > minpix:
-			leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-		if len(good_right_inds) > minpix:		
-			rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+		# Step through the windows one by one
+		for window in range(nwindows):
+			# Identify window boundaries in x and y (and right and left)
+			win_y_low = binary_warped.shape[0] - (window+1)*window_height
+			win_y_high = binary_warped.shape[0] - window*window_height
+			win_xleft_low = leftx_current - margin
+			win_xleft_high = leftx_current + margin
+			win_xright_low = rightx_current - margin
+			win_xright_high = rightx_current + margin
+			# Draw the windows on the visualization image
+			cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+			cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+			# Identify the nonzero pixels in x and y within the window
+			good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+			good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+			# Append these indices to the lists
+			left_lane_inds.append(good_left_inds)
+			right_lane_inds.append(good_right_inds)
+			# If you found > minpix pixels, recenter next window on their mean position
+			if len(good_left_inds) > minpix:
+				leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+			if len(good_right_inds) > minpix:		
+				rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
 
-	# Concatenate the arrays of indices
-	left_lane_inds = np.concatenate(left_lane_inds)
-	right_lane_inds = np.concatenate(right_lane_inds)
+		# Concatenate the arrays of indices
+		left_lane_inds = np.concatenate(left_lane_inds)
+		right_lane_inds = np.concatenate(right_lane_inds) 
 
-	# Extract left and right line pixel positions
+	else:
+		# Assume you now have a new warped binary image 
+		# from the next frame of video (also called "binary_warped")
+		# It's now much easier to find line pixels!
+		left_lane_inds = ((nonzerox > (lane.left_fit[0]*(nonzeroy**2) + lane.left_fit[1]*nonzeroy + lane.left_fit[2] - margin)) & (nonzerox < (lane.left_fit[0]*(nonzeroy**2) + lane.left_fit[1]*nonzeroy + lane.left_fit[2] + margin))) 
+		right_lane_inds = ((nonzerox > (lane.right_fit[0]*(nonzeroy**2) + lane.right_fit[1]*nonzeroy + lane.right_fit[2] - margin)) & (nonzerox < (lane.right_fit[0]*(nonzeroy**2) + lane.right_fit[1]*nonzeroy + lane.right_fit[2] + margin)))  
+
+	# Again, extract left and right line pixel positions
 	leftx = nonzerox[left_lane_inds]
 	lefty = nonzeroy[left_lane_inds] 
 	rightx = nonzerox[right_lane_inds]
-	righty = nonzeroy[right_lane_inds] 
-
+	righty = nonzeroy[right_lane_inds]
 	# Fit a second order polynomial to each
 	left_fit = np.polyfit(lefty, leftx, 2)
 	right_fit = np.polyfit(righty, rightx, 2)
+	
+	if is_video:
+		lane.detected = True
+		lane.left_fit = left_fit
+		lane.right_fit = right_fit
 	
 	# Generate x and y values for plotting
 	ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
 	left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
 	right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
-	# Uncomment for saving image
-	"""
 	out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
 	out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 	plt.imshow(out_img)
@@ -177,11 +175,12 @@ def detect_lines(binary_warped, filename):
 	plt.plot(right_fitx, ploty, color='yellow')
 	plt.xlim(0, binary_warped.shape[1])
 	plt.ylim(binary_warped.shape[0], 0)
-	write_name = output_folder + 'sliding_windows_' + filename
-	plt.savefig(write_name)
-	plt.clf()
-	"""
 	
+	if save_images:
+		write_name = output_folder + 'sliding_windows_' + filename
+		plt.savefig(write_name)
+		plt.clf()
+
 	# Calculate curvature
 	y_eval = np.max(ploty)
 	
@@ -196,13 +195,15 @@ def detect_lines(binary_warped, filename):
 	left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
 	right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
 	# Now our radius of curvature is in meters
+	curvature = (left_curverad + right_curverad) / 2
 	
 	# Create an image to draw the lines on
 	warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
 	color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
 	lane_center = (right_fitx[0] - left_fitx[0])/2 + left_fitx[0]
-	print(round(lane_center - 640,3))
-
+	lane_center -= 640
+	lane_center_meters = lane_center*xm_per_pix
+	
 	# Recast the x and y points into usable format for cv2.fillPoly()
 	pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
 	pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
@@ -211,33 +212,12 @@ def detect_lines(binary_warped, filename):
 	# Draw the lane onto the warped blank image
 	cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
 	
-	return color_warp, left_curverad, right_curverad
+	return color_warp, curvature, lane_center_meters
+
+def pipeline(img):
 	
-def detect_new_frame_lines(binary_warped):
-	# Assume you now have a new warped binary image 
-	# from the next frame of video (also called "binary_warped")
-	# It's now much easier to find line pixels!
-	nonzero = binary_warped.nonzero()
-	nonzeroy = np.array(nonzero[0])
-	nonzerox = np.array(nonzero[1])
-	margin = 100
-	left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin))) 
-	right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))  
+	global lane
 
-	# Again, extract left and right line pixel positions
-	leftx = nonzerox[left_lane_inds]
-	lefty = nonzeroy[left_lane_inds] 
-	rightx = nonzerox[right_lane_inds]
-	righty = nonzeroy[right_lane_inds]
-	# Fit a second order polynomial to each
-	left_fit = np.polyfit(lefty, leftx, 2)
-	right_fit = np.polyfit(righty, rightx, 2)
-	# Generate x and y values for plotting
-	ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-	left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-	right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
-def pipeline(img, filename):
 	img = np.copy(img)
 	
 	# STEP A.2 UNDISTORT
@@ -250,33 +230,36 @@ def pipeline(img, filename):
 	result, Minv = transform(result)
 	
 	# Uncomment for saving file
-	"""
-	# Save binary file
-	final_image_RGB = out_img = np.dstack((result, result, result))*255
-	write_name = output_folder + 'binary_warp_' + filename
-	cv2.imwrite(write_name,final_image_RGB)
-	"""
 	
+	if save_images:
+		# Save binary file
+		final_image_RGB = out_img = np.dstack((result, result, result))*255
+		write_name = output_folder + 'binary_warp_' + filename
+		cv2.imwrite(write_name,final_image_RGB)
+	
+
 	# STEP B Detect lane lines and curvature
-	color_warp, left_curverad, right_curverad = detect_lines(result, filename)
+	color_warp, curvature, lane_center_meters = detect_lines(result,lane)
 	
 	# Warp the blank back to original image space using inverse perspective matrix (Minv)
 	newwarp = cv2.warpPerspective(color_warp, Minv, (img.shape[1], img.shape[0])) 
 	# Combine the result with the original image
 	result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
-	text = ('Radius of curvature: ' + str(round(left_curverad,3)) + ' m ' + str(round(right_curverad,3)) + ' m')
+	text = ('Radius of curvature: ' + str(round(curvature,3)) + ' m')
 	font = cv2.FONT_HERSHEY_SIMPLEX
-	cv2.putText(result,text,(300,70), font, 1,(255,255,255),2)   
+	cv2.putText(result,text,(300,70), font, 1,(255,255,255),2)
+	text = ('Distance from lane center: ' + str(round(lane_center_meters,3)) + ' m')
+	cv2.putText(result,text,(300,120), font, 1,(255,255,255),2)   
 	
-	# Uncomment for saving file
-
-	# Save final image
-	write_name = output_folder + 'final_image_' + filename
-	cv2.imwrite(write_name,result)
+	if save_images:
+		# Save final image
+		write_name = output_folder + 'final_image_' + filename
+		cv2.imwrite(write_name,result)
 	
 	return result
-
-
+	
+lane = Lane()
+	
 for fname in images:
 
 	filename = os.path.basename(fname)
@@ -285,21 +268,33 @@ for fname in images:
 	# Read in an image
 	img = cv2.imread(fname)
 	
-	# Uncomment for saving file
-	"""
-	# Warp original
-	undist = cv2.undistort(img, mtx, dist, None, mtx)
-	warped, Minv = transform(undist)
-	write_name = output_folder + 'warped_' + filename
-	cv2.imwrite(write_name,warped)
-	"""
+	if save_images:
+		# Warp original
+		undist = cv2.undistort(img, mtx, dist, None, mtx)
+		warped, Minv = transform(undist)
+		write_name = output_folder + 'warped_' + filename
+		cv2.imwrite(write_name,warped)
 
 	# Process the image
-	result = pipeline(img, filename)
+	result = pipeline(img)
 	
 	# Save output
 	#cv2.imshow('Original image', img)
 	#cv2.imshow('Result', result)
-	#cv2.waitKey(1000)
+	#cv2.waitKey(1000
+	#cv2.destroyAllWindows()
 	
-cv2.destroyAllWindows()
+# Import everything needed to edit/save/watch video clips
+from moviepy.editor import VideoFileClip
+
+save_images = False
+is_video = True
+white_output = 'project_video_result.mp4'
+## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+## To do so add .subclip(start_second,end_second) to the end of the line below
+## Where start_second and end_second are integer values representing the start and end of the subclip
+## You may also uncomment the following line for a subclip of the first 5 seconds
+##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
+clip1 = VideoFileClip("project_video.mp4")
+white_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
+white_clip.write_videofile(white_output, audio=False)
